7d70fbc01b22133d6e3e80032148c57e4db9652b
ElijahKhin <workbiscap@gmail.com>
2020-09-23 12:35:55 +0300

    I increased the speed of the algorithm by 8 times

--- code_wars.py
+++ code_wars.py
@@ -5,48 +5,37 @@ start_time = time.time()
 
 
 def solve(n):
+
+    lst = []
+    add = 1
+    do = True
     if n == 1:  # exception 1
         return 2
     elif n == 2:  # exception 2
         return 3
 
-    score = 0
-    number_is_found = False
-
     for pls in range(n - 1, 1, -1):  # 'pls' - prime in left side
         for divider in range(2, round(pls / 2) + 1):
-            if pls % divider != 0:
-                score += 1
-            else:
+            if pls % divider == 0:
                 break
-            if score == len(range(2, round(pls / 2) + 1)):
-                # print(f'{pls} <------- this number is the nearest prime! (left side)')
-                number_is_found = True
-        if number_is_found:
+        else:
+            lst.append(pls)
             break
 
-    score = 0
-    number_is_found = False
-    prs = n  # 'prs' - prime in right side
-    while number_is_found is not True:
-        prs += 1
-        for divider in range(2, round(prs / 2) + 1):
-            if prs % divider != 0:
-                score += 1
-            else:
+    while do is True:
+        add += 1
+        for divider in range(2, round((n+add) / 2) + 1):
+            if (n + add) % divider == 0:
                 break
-            if score == len(range(2, round(prs / 2) + 1)):
-                # print(f'{prs} <------- this number is the nearest prime! (right side)')
-                number_is_found = True
+        else:
+            lst.append(n+add)
+            break
 
-    if prs - n > n - pls:
-        return pls
-    elif prs - n == n - pls:
-        return pls
-    else:
-        return prs
+    print(lst)
 
 
-print(solve(500))
+solve(8000000)
 
-print(time.time() - start_time, 'second_exp')
+
+print(time.time() - start_time, 'new_alg')
+

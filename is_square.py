70921fbb843fffab3214f099e559208e1cb2b2fb
ElijahKhin <workbiscap@gmail.com>
2020-09-25 00:36:10 +0300

    I improved this algorithm

--- is_square.py
+++ is_square.py
@@ -1,9 +1,7 @@
 def is_square(n=0):
-    if n < 0:
-        return False
-    if n**(1/2) - int(n**(1/2)) == 0:
-        return True
-    else:
-        return False
+    return n > 0 and n**(1/2) - int(n**(1/2)) == 0
+
+
+print(is_square(-9))
 
 

73330c7ce569eee8775ead87164193426245d9e1
ElijahKhin <workbiscap@gmail.com>
2020-09-26 01:23:19 +0300

    Solved one more kata

---
+++ maskify.py
@@ -0,0 +1,40 @@
+def maskify(cc):
+    if cc == '':
+        return ''
+    elif len(cc) < 4:
+        return cc
+    lst = []
+
+    for x in range(len(cc[:-4])):
+        k = '#'
+        lst.append(k)
+    for x in range(len(cc)-4, len(cc)):
+        k = cc[x]
+        lst.append(k)
+    return ''.join(lst)
+
+
+print(maskify('123'))
+
+
+def maskify_not_mine(cc):
+    return "#"*(len(cc)-4) + cc[-4:]
+
+print(maskify_not_mine('123'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

75f62dc2d5bd0ef6123b9cc355cae24d5e1535c7
ElijahKhin <workbiscap@gmail.com>
2020-09-25 20:13:04 +0300

    Solved new kata, added a description

--- is_square.py
+++ is_square.py
@@ -1,3 +1,10 @@
+# Elijah Khin
+# workbiscap@gmail.com
+
+# Description:
+# I will be given a number and then I'll must answer a question: Is it square number?
+
+
 def is_square(n=0):
     return n > 0 and n**(1/2) - int(n**(1/2)) == 0
 
---
+++ min_string.py
@@ -0,0 +1,34 @@
+# Elijah Khin
+# workbiscap@gmail.com
+
+# Description:
+# My task is to return a min word in input string.
+
+
+
+def find_short(s):
+
+    word_length = 0
+    all_words = []
+    for i in range(len(s)):
+        if s[i] != " ":
+            word_length += 1
+        else:
+            all_words.append(word_length)
+            word_length = 0
+    else:
+        all_words.append(word_length)
+    for i in range(len(all_words)):
+        for j in range(len(all_words)):
+            if all_words[i] > all_words[j]:
+                break
+        else:
+            return all_words[i]
+
+
+def find_short1(s):
+    return len(min(s.split(' '), key=len))
+
+
+def find_short2(s):
+    return min(len(x) for x in s.split())
